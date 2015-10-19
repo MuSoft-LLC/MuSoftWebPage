a530d00b0d7b6ad957b5330ce03390bfa64c216c
Nick <nickmuscara@gmail.com>
2015-10-18 21:43:39 -0700

    -m "blog and blog entry finished. some misc. tweaks"

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 	// create the module and name it scotchApp
-	var MuSoft = angular.module('MuSoft', ['ngRoute',"firebase"]);
+	var MuSoft = angular.module('MuSoft', ['ngRoute','firebase', 'ngAnimate', 'ui.bootstrap' ]);
 
 	// configure our routes
 	MuSoft.config(function($routeProvider) {
@@ -62,7 +62,7 @@
 	});
 	
 	MuSoft.controller('tipCalcController', function($scope) {
-		$scope.message = 'Tip Calculator';
+		$scope.message = 'A Simple Tip Calculator and Bill Splitter';
 	});
 	
 	MuSoft.controller('blogController', function($scope) {
@@ -73,21 +73,47 @@
 		$scope.message = 'This is how you post a blog entry';
 	});
 	
-	MuSoft.controller("firebaseController", function($scope, $firebaseArray) {
+  MuSoft.controller("firebaseController", function($scope, $firebaseArray, $interval) {
   		var ref = new Firebase("https://intense-heat-9971.firebaseio.com/");
   // create a synchronized array
-  $scope.blogs = $firebaseArray(ref);
+  	$scope.blogs = $firebaseArray(ref);
+	  
+	//$scope.newBlogDateTime = document.getElementById("lblTime").innerHTML = Date();  
+	
+	var tick = function() {
+		$scope.clock = Date.now();
+	}
+	tick();
+	$interval(tick,1000);
+	
   // add new items to the array
   // the message is automatically added to our Firebase database!
-  $scope.addBlog = function() {
-    $scope.blogs.$add({
-      title: $scope.newBlogTitle,
-	  author: $scope.newBlogAuthor,
-	  body: $scope.newBlogBody
-	  
+  	$scope.addBlog = function() {			
+		$scope.blogs.$add({
+    	  blog:{
+		  title: $scope.newBlogTitle,
+		  author: $scope.newBlogAuthor,
+		  body: $scope.newBlogBody,
+		  dateTime: $scope.clock
+		  }
+		  });
+  	};
+  });
+  MuSoft.controller("carouselController", function($scope) {
+	  $scope.myInterval = 5000;
+  	  $scope.noWrapSlides = false;
+  	  var slides = $scope.slides = [];
+  	  $scope.addSlide = function() {
+      var newWidth = 600 + slides.length + 1;
+      slides.push({
+      image: '/../img/' + newWidth,
+      text: ['Logo','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
     });
   };
-
-});
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
+  });
 
           
