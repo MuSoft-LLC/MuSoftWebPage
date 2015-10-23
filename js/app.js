3bd64a9d19cf7d6da8e34d6dd5e57f557b15ec40
Nick <nickmuscara@gmail.com>
2015-10-22 23:59:23 -0700

    Contact Modal Form Markup

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-	// create the module and name it scotchApp
+
 	var MuSoft = angular.module('MuSoft', ['ngRoute','firebase', 'ngAnimate', 'ui.bootstrap' ]);
 
 	// configure our routes
@@ -57,9 +57,6 @@
 		$scope.message = 'Look! I am an about page.';
 	});
 
-	MuSoft.controller('contactController', function($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
-	});
 	
 	MuSoft.controller('tipCalcController', function($scope) {
 		$scope.message = 'A Simple Tip Calculator and Bill Splitter';
@@ -74,18 +71,17 @@
 	});
 	
   MuSoft.controller("firebaseController", function($scope, $firebaseArray, $interval) {
-  		var ref = new Firebase("https://intense-heat-9971.firebaseio.com/");
-  // create a synchronized array
+  	var ref = new Firebase("https://intense-heat-9971.firebaseio.com/");
+    // create a synchronized array
   	$scope.blogs = $firebaseArray(ref);
 	  
-	//$scope.newBlogDateTime = document.getElementById("lblTime").innerHTML = Date();  
-	
+	//$scope.newBlogDateTime = document.getElementById("lblTime").innerHTML = Date();  	
 	var tick = function() {
 		$scope.clock = Date.now();
 	}
 	tick();
 	$interval(tick,1000);
-	
+
   // add new items to the array
   // the message is automatically added to our Firebase database!
   	$scope.addBlog = function() {			
@@ -98,22 +94,60 @@
 		  
 		  });
   	};
-  });
-  MuSoft.controller("carouselController", function($scope) {
-	  $scope.myInterval = 5000;
-  	  $scope.noWrapSlides = false;
-  	  var slides = $scope.slides = [];
-  	  $scope.addSlide = function() {
-      var newWidth = 600 + slides.length + 1;
-      slides.push({
-      image: '/../img/' + newWidth,
-      text: ['Logo','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+  });  
+  MuSoft.controller('contactController',
+   function ($scope, $uibModal, $log) {
+
+  $scope.message = 'Contact us! JK. This is just a demo.';
+  
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'contactModal.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      console.log('Modal dismissed at: ' + new Date());
     });
   };
-  for (var i=0; i<4; i++) {
-    $scope.addSlide();
-  }
-  });
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+});
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+
+MuSoft.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
 
           
